fb9842f3dfcf7098fb8a369866cc8176488c5c3b
yihhao wang <2633565580@qq.com>
2024-03-11 21:31:32 +0800

    feat(data): finished database connnection ini

--- data/data.go
+++ data/data.go
@@ -1,1 +1,32 @@
 package data
+
+import (
+	"context"
+	"fmt"
+	"github.com/dstgo/tracker/conf"
+	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func LoadGormDB(dbConf conf.DBConf) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbConf.User, dbConf.Password, dbConf.Address, dbConf.Params)
+	dialector := mysql.Open(dsn)
+	db, err := gorm.Open(dialector)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func LoadRedisDB(rdConf conf.RedisConf) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     rdConf.Address,
+		Password: rdConf.Auth,
+	})
+	pingRes := client.Ping(context.Background())
+	if err := pingRes.Err(); err != nil {
+		return nil, err
+	}
+	return client, nil
+}
