55b22c32e58652f20881ababe5c93e8689580f21
yihhao wang <2633565580@qq.com>
2024-03-12 21:44:48 +0800

    feat(server): finished http server initialization

---
+++ server/http.go
@@ -0,0 +1,108 @@
+package server
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/middlewares/server/recovery"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/dstgo/tracker/conf"
+	"github.com/dstgo/tracker/types"
+	"github.com/go-redis/redis/v8"
+	"github.com/hertz-contrib/cache"
+	"github.com/hertz-contrib/cache/persist"
+	"github.com/hertz-contrib/limiter"
+	"github.com/hertz-contrib/logger/accesslog"
+	"github.com/hertz-contrib/requestid"
+	"net/http"
+	"time"
+	_ "time/tzdata"
+)
+
+// returns a new hertz http server
+func newHttpServer(httpConf conf.HttpConf, client *redis.Client) (*server.Hertz, error) {
+	hertz := server.New(
+		server.WithHostPorts(httpConf.Listen),
+		server.WithReadTimeout(httpConf.ReadTimeout),
+		server.WithIdleTimeout(httpConf.IdleTimeout),
+		server.WithBasePath(httpConf.BasePath),
+	)
+
+	logHandler, err := accessLogHandler()
+	if err != nil {
+		return nil, err
+	}
+
+	hertz.Use(
+		// recovery handler
+		recoveryHandler(),
+		// request limiter
+		limiterHandler(),
+		// X-Request-ID
+		requestid.New(),
+		// log handler
+		logHandler,
+		// cache handler
+		cacheHandler(client, httpConf),
+	)
+
+	return hertz, nil
+}
+
+func accessLogHandler() (app.HandlerFunc, error) {
+	accesslog.Tags["requestId"] = func(output accesslog.Buffer, c *app.RequestContext, data *accesslog.Data, extraParam string) (int, error) {
+		requestId := c.Response.Header.Get("X-Request-ID")
+		return output.WriteString(requestId)
+	}
+
+	// timezone
+	location, err := time.LoadLocation("Asia/shanghai")
+	if err != nil {
+		return nil, err
+	}
+
+	// format string
+	format := "[${time}] ${status} - ${latency} ${method} ${path} ${url} ${ip} ${ua}"
+	timeformat := "2006-01-02 15:04:05.000000"
+
+	return accesslog.New(
+		accesslog.WithTimeZoneLocation(location),
+		accesslog.WithFormat(format),
+		accesslog.WithTimeFormat(timeformat),
+	), nil
+}
+
+func recoveryHandler() app.HandlerFunc {
+	return recovery.Recovery(
+		recovery.WithRecoveryHandler(func(c context.Context, ctx *app.RequestContext, err interface{}, stack []byte) {
+			hlog.DefaultLogger().CtxErrorf(c, "[Recovery] err=%v\nstack=%s", err, stack)
+			hlog.DefaultLogger().Infof("Client: %s", ctx.Request.Header.UserAgent())
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+		}),
+	)
+}
+
+func cacheHandler(redisCli *redis.Client, httpConf conf.HttpConf) app.HandlerFunc {
+	store := persist.NewRedisStore(redisCli)
+	cacheH := cache.NewCacheByRequestURIWithIgnoreQueryOrder(store, httpConf.CacheTTL, cache.WithPrefixKey("tracker-cache-"))
+	return cacheH
+}
+
+func limiterHandler() app.HandlerFunc {
+	return func(c context.Context, ctx *app.RequestContext) {
+		limiter.AdaptiveLimit()
+		newLimiter := limiter.NewLimiter()
+		done, err := newLimiter.Allow()
+		if err != nil {
+			ctx.AbortWithStatusJSON(consts.StatusTooManyRequests, types.Response{
+				Code: consts.StatusTooManyRequests,
+				Data: nil,
+				Msg:  "too many requests",
+			})
+		} else {
+			ctx.Next(c)
+			done()
+		}
+	}
+}
--- server/server.go
+++ server/server.go
@@ -3,11 +3,13 @@ package server
 import (
 	"context"
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/dstgo/lobbyapi"
 	"github.com/dstgo/steamapi"
 	"github.com/dstgo/tracker/api"
 	"github.com/dstgo/tracker/conf"
 	"github.com/dstgo/tracker/data"
+	hertzslog "github.com/hertz-contrib/logger/slog"
 	"io"
 	"log/slog"
 	"os"
@@ -21,11 +23,11 @@ func NewTracker(ctx context.Context, file string) (*server.Hertz, error) {
 		return nil, err
 	}
 
-	logger, closer, err := newLogger(appConf.Log)
+	// load logger
+	closer, err := loadLogger(appConf.Log)
 	if err != nil {
 		return nil, err
 	}
-	slog.SetDefault(logger)
 
 	// load mongodb
 	mgodb, err := data.LoadMongoDB(ctx, appConf.DB)
@@ -39,7 +41,7 @@ func NewTracker(ctx context.Context, file string) (*server.Hertz, error) {
 	}
 
 	// new server
-	hertz, err := newHttpServer(appConf.Http)
+	hertz, err := newHttpServer(appConf.Http, redisDB)
 	if err != nil {
 		return nil, err
 	}
@@ -69,45 +71,34 @@ func NewTracker(ctx context.Context, file string) (*server.Hertz, error) {
 	return hertz, nil
 }
 
-// returns a new hertz http server
-func newHttpServer(httpConf conf.HttpConf) (*server.Hertz, error) {
-	hertz := server.New(
-		server.WithHostPorts(httpConf.Listen),
-		server.WithReadTimeout(httpConf.ReadTimeout),
-		server.WithIdleTimeout(httpConf.IdleTimeout),
-		server.WithBasePath(httpConf.BasePath),
-	)
-
-	return hertz, nil
-}
-
 // returns a new logger
-func newLogger(logConf conf.LogConf) (*slog.Logger, io.Closer, error) {
+func loadLogger(logConf conf.LogConf) (io.Closer, error) {
 	dir := filepath.Dir(logConf.File)
 	if dir != "." && len(dir) > 0 {
 		err := os.MkdirAll(dir, 0666)
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
 	}
 
 	// open log file
 	logFile, err := os.OpenFile(logConf.File, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	writer := io.MultiWriter(os.Stdout, logFile)
 	// set level
-	var level slog.Level
+	var level = new(slog.LevelVar)
 	if err := level.UnmarshalText([]byte(logConf.Level)); err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	// new logger
-	logger := slog.New(slog.NewTextHandler(writer, &slog.HandlerOptions{
-		AddSource: false,
-		Level:     level,
-	}))
-	return logger, nil, nil
+	// set logger
+	hlog.SetLogger(hertzslog.NewLogger(
+		hertzslog.WithLevel(level),
+		hertzslog.WithOutput(writer),
+	))
+
+	return logFile, nil
 }
