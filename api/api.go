afaf0208bdd3289aa8bda6e25191545b3cc1e54d
yihhao wang <2633565580@qq.com>
2024-03-16 19:10:22 +0800

    feat(api): finished api register

---
+++ api/api.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/dstgo/tracker/internal/data/repo"
+	"github.com/dstgo/tracker/internal/handler"
+	"github.com/dstgo/tracker/internal/types"
+)
+
+type API struct {
+	Sys   SystemAPI
+	Lobby LobbyAPI
+}
+
+// NewRouter registers http router handlers
+func NewRouter(ctx context.Context, hertz *server.Hertz, env *types.Env) (*API, error) {
+
+	hlog.Debug("initializing data repo and creating db index")
+	// repositories
+	lobbyRepo, err := repo.NewLobbyRepo(ctx, env.MongoDB)
+	if err != nil {
+		return nil, err
+	}
+
+	// handler
+	lobbyMongoHandler := handler.NewLobbyMongoHandler(lobbyRepo, env.RedisDB, env.LobbyCLI, env.GeoIpDB)
+
+	// sys api
+	sysAPI := SystemAPI{}
+	hertz.GET("/ts", sysAPI.Ts)
+
+	// lobby api
+	lobbyAPI := LobbyAPI{LobbyHandler: lobbyMongoHandler}
+	hertz.GET("/lobby/list", lobbyAPI.List)
+	hertz.GET("/lobby/details", lobbyAPI.Details)
+
+	return &API{
+		Sys:   sysAPI,
+		Lobby: lobbyAPI,
+	}, nil
+}
